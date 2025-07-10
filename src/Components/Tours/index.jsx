4692d2e101564427aa9acad4e161c3f11ca37efe
unknown <misaelzion@gmail.com>
2025-07-10 16:55:02 -0600

    llenado de tours

--- src/Components/Tours/index.jsx
+++ src/Components/Tours/index.jsx
@@ -1,22 +1,24 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import contentData from '../../data/content.json';
 import { ToursContext } from "../../Context";
-import toursData from '../../data/tours.json';
 import { motion } from 'framer-motion'
 import { Link } from "react-router-dom";
 
 
 const categories = [
-    { name: 'Sea Adventure', image: './src/assets/images/sea.jpg', category: 'sea' },
-    { name: 'Yatch & Boat', image: './src/assets/images/boat.jpg', category: 'boat' },
-    { name: 'Land Adventure', image: './src/assets/images/land.jpg', category: 'land' },
-    { name: 'City Tour', image: './src/assets/images/city.jpg', category: 'city' },
+  { key: 'sunsetCruises', image: './src/assets/images/sunset.jpg' },
+  { key: 'snorkelDiving', image: './src/assets/images/snorkel.jpg' },
+  { key: 'boatYatch', image: './src/assets/images/boat.jpg' },
+  { key: 'whaleWatching', image: './src/assets/images/sea.jpg' },
+  { key: 'camelHorse', image: './src/assets/images/camel.jpg' },
+  { key: 'atvRazors', image: './src/assets/images/utv.jpg' },
+  { key: 'dayTrip', image: './src/assets/images/land.jpg' },
 ];
+
 
 const Tours = () =>{    
     const { language } = useContext(ToursContext);
-    const [tours, setTours] = useState([]);
-    const content = contentData[language] || contentData['en']; // fallback to English
+    const content = contentData[language] || contentData['en']; 
     return (     
         <section id="tours" className="w-full bg-gradient-to-l from-[#03A6A6] to-[#07bcbc] px-10 py-5 scroll-mt-20">
         <motion.div
@@ -28,29 +30,24 @@ const Tours = () =>{
         >
             <h2 className="font-[outfit] font-semibold text-[2.5rem] sm:text-[3.5rem] text-white text-center sm:text-left">{content.toursTitle}</h2>
         </motion.div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3  gap-6 mt-4">
             {categories.map((category, index) => (
                 <div
                     key={index}
                     className="relative w-full h-60 rounded-xl overflow-hidden shadow-lg group cursor-pointer"
                 >
-
-                     <Link to={`/Tours/${category.category}`}>
-
+                    <Link to={`/Tours/${category.key}`}>
                        <img
                         src={category.image}
-                        alt={category.name}
-                        className="w-full h-full object-cover transform group-hover:scale-105 transition duration-500"
+                        alt={content.categories[category.key]}
+                        className="w-full h-[80%] object-cover transform group-hover:scale-105 transition duration-500"
                     />
                     <div className="absolute inset-0 flex items-end justify-center">
                         <div className="w-full bg-white bg-opacity-90 text-center py-3 text-[#03A6A6] font-semibold text-xl">
-                            {category.name}
+                            {content.categories[category.key]}
                         </div>
                     </div>
-
-
-                     </Link>
-                  
+                    </Link>            
                 </div>
             ))}
         </div>
--- src/Pages/Tours/index.jsx
+++ src/Pages/Tours/index.jsx
@@ -6,42 +6,38 @@ import { ToursContext } from "../../Context";
 import "leaflet/dist/leaflet.css";
 import "leaflet/dist/leaflet.css";
 import 'react-multi-carousel/lib/styles.css';
-import seaImg from '../../assets/images/sea.jpg';
-import boatImg from '../../assets/images/boat.jpg';
-import landImg from '../../assets/images/land.jpg';
-import cityImg from '../../assets/images/city.jpg';
+import contentData from '../../data/content.json';
+import { CalendarDaysIcon, ClockIcon, CurrencyDollarIcon } from '@heroicons/react/24/outline'
+
 
 const categories = [
-    { name: 'Sea Adventure', image: seaImg, category: 'sea' },
-    { name: 'Yatch & Boat', image: boatImg, category: 'boat' },
-    { name: 'Land Adventure', image: landImg, category: 'land' },
-    { name: 'City Tour', image: cityImg, category: 'city' },
+  { key: 'sunsetCruises', image: './src/assets/images/sunset.jpg' },
+  { key: 'snorkelDiving', image: './src/assets/images/snorkel.jpg' },
+  { key: 'boatYatch', image: './src/assets/images/boat.jpg' },
+  { key: 'whaleWatching', image: './src/assets/images/sea.jpg' },
+  { key: 'camelHorse', image: './src/assets/images/camel.jpg' },
+  { key: 'atvRazors', image: './src/assets/images/utv.jpg' },
+  { key: 'dayTrip', image: './src/assets/images/land.jpg' },
 ];
+
 function tours() {  
     const { language } = useContext(ToursContext);
+    const content = contentData[language] || contentData['en']; 
     const { id } = useParams();  
     const filteredTours = toursData.tours.filter(tour => tour.category === id);
     if (!filteredTours) {
         return <div>Tours not found</div>;
     }
-
-console.log(id);
-  const categoryData = categories.find(cat => cat.category.toLowerCase() === id.toLowerCase());
-console.log(categoryData.name);
+    const categoryData = categories.find(cat => cat.key.toLowerCase() === id.toLowerCase());
     return (
-        <Layout>
-              
-              <div
+        <Layout>              
+        <div
         className="relative h-[270px] w-full flex items-center justify-center bg-cover bg-center"
-     
-
         style={{ backgroundImage: `url(${categoryData?.image})` }}
-
-
       >
         <div className="absolute inset-0 bg-black/40"></div>
         <h1 className="relative z-10 text-white text-4xl font-semibold uppercase">
-          {categoryData?.name || id} Tours
+          {content.categories[categoryData.key]}
         </h1>
       </div>
 
@@ -61,9 +57,9 @@ console.log(categoryData.name);
               />
               <div className="p-4 space-y-2">
                 <h2 className="text-lg font-semibold text-[#03A6A6]">{t.title}</h2>
-                <p className="text-sm text-gray-600">{t.duration}</p>
-                <p className="text-sm text-gray-600">{t.period}</p>
-                <p className="text-sm text-gray-600">{t.price}</p>
+                <div className="flex items-center gap-1"><ClockIcon aria-hidden="true" className="shrink-[0] block size-5 text-[#03A6A6]"/><p className="text-sm text-gray-600">{t.duration}</p></div>
+                <div className="flex items-center gap-1"><CalendarDaysIcon aria-hidden="true" className="shrink-[0] block size-5 text-[#03A6A6]"/><p className="text-sm text-gray-600">{t.period}</p></div>
+                <div className="flex items-center gap-1"><CurrencyDollarIcon aria-hidden="true" className="shrink-[0] block size-5 text-[#03A6A6]"/><p className="text-sm text-gray-600">{t.price}</p></div>
               </div>
             </div>
           );
