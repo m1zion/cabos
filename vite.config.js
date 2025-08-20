e8666cd83f5be3cd395a039b02442daa15d6a513
unknown <misaelzion@gmail.com>
2025-08-20 16:52:06 -0600

    prueba imagenes4

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 import svgr from "vite-plugin-svgr"; //Para colorear los svg
 
 // https://vite.dev/config/
+//Para hacer el build en github pages corremos este comando, de esta manera pondra la base necesaria
+//GITHUB_PAGES=true npm run build
+
 const isGithubPages = process.env.GITHUB_PAGES === 'true'
 export default defineConfig({
   plugins: [
