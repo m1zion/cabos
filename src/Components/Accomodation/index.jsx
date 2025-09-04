5b2e1ada4affc024fcb312f2251f8f20db78e4cb
unknown <misaelzion@gmail.com>
2025-09-03 18:44:22 -0600

    new experiences

--- src/Components/Accomodation/index.jsx
+++ src/Components/Accomodation/index.jsx
@@ -59,37 +59,39 @@ const Accomodation = () =>{
                 return (
                     <div key={accomodation.id} className="w-[100%]  border border-gray-100 bg-white rounded-sm shadow-lg overflow-hidden transform transition duration-400 hover:-translate-y-1">
                         <img src={withBase(accomodation.images.portrait)} alt={t.title} className="w-full h-60 object-cover" />
-                        <div className="text-[#256A77] p-4 space-y-2">
-                            <div className="flex justify-between pr-[5px] items-center">                               
-                                <h3 className="text-md md:text-xl font-semibold text-[1.1rem]">{t.title}</h3>    
-                            </div>
-                            <div className="flex gap-2"> 
-                                <MapPinIcon  className="block size-5" />
-                                <p className="text-gray-600">{t.location}</p> 
-                            </div>                            
-                           <div className="pl-[3px] flex items-center gap-4 text-gray-500 text-[1rem] mt-2">
-                                {RoomIcon && (
-                                    <div className="flex items-center gap-1">
-                                    <img src={RoomIcon} alt="rooms" className="w-4 h-4" />
-                                    <span>{t.beds}</span>
-                                    <span className="pl-2">•</span>
-                                    </div>
-                                )}
-                                {BathIcon && (
-                                    <div className="flex items-center gap-1">
-                                    <img src={BathIcon} alt="bathrooms" className="w-4 h-4" />
-                                    <span>{t.bathrooms}</span>
-                                    </div>
-                                )}
-                                {(ParkingIcon && t.parking != '') && (
-                                    <div className="flex items-center gap-1">
-                                    <span className="pr-2">•</span> <img src={ParkingIcon} alt="parking" className="w-4 h-4" />
-                                    <span>{t.parking}</span>
-                                    </div>
-                                )}
-                            </div>   
+                        <div className="flex-column h-full text-[#256A77] p-4 space-y-2">
+                            <div >
+                                <div className="justify-between pr-[5px] items-center">                               
+                                    <h3 className="text-md md:text-xl font-semibold text-[1.1rem]">{t.title}</h3>    
+                                </div>
+                                <div className="flex gap-2"> 
+                                    <MapPinIcon  className="block size-5" />
+                                    <p className="text-gray-600">{t.location}</p> 
+                                </div>                            
+                                <div className="pl-[3px] flex items-center gap-4 text-gray-500 text-[1rem] mt-2">
+                                    {RoomIcon && (
+                                        <div className="flex items-center gap-1">
+                                        <img src={RoomIcon} alt="rooms" className="w-4 h-4" />
+                                        <span>{t.beds}</span>
+                                        <span className="pl-2">•</span>
+                                        </div>
+                                    )}
+                                    {BathIcon && (
+                                        <div className="flex items-center gap-1">
+                                        <img src={BathIcon} alt="bathrooms" className="w-4 h-4" />
+                                        <span>{t.bathrooms}</span>
+                                        </div>
+                                    )}
+                                    {(ParkingIcon && t.parking != '') && (
+                                        <div className="flex items-center gap-1">
+                                        <span className="pr-2">•</span> <img src={ParkingIcon} alt="parking" className="w-4 h-4" />
+                                        <span>{t.parking}</span>
+                                        </div>
+                                    )}
+                                </div>  
+                            </div>                             
                             <Link to={`/Accommodation/${accomodation.id}`}>
-                                <button className="hover:bg-[#EEF4F3] mt-2 px-4 py-2 border border-[#256A77] text-[#256A77] rounded-md cursor-pointer">
+                                <button className="w-full hover:bg-[#EEF4F3] mt-2 px-4 py-3 border border-[#256A77] text-[#256A77] rounded-md cursor-pointer">
                                     {content.viewListing}
                                 </button>
                             </Link>
