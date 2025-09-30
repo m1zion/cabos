267922e8dd084cee9dc04e4c88af13fb70f80824
unknown <misaelzion@gmail.com>
2025-09-29 18:51:20 -0600

    imagenes

--- src/Pages/Places/index.jsx
+++ src/Pages/Places/index.jsx
@@ -5,23 +5,30 @@ import { useParams } from 'react-router-dom';
 import { ToursContext } from "../../Context";
 import "leaflet/dist/leaflet.css";
 import "leaflet/dist/leaflet.css";
-import 'react-multi-carousel/lib/styles.css';
 import contentData from '../../data/content.json';
-import { CalendarDaysIcon, ClockIcon, CurrencyDollarIcon } from '@heroicons/react/24/outline'
-import { Link } from "react-router-dom";
+import { CalendarDaysIcon, ChevronLeftIcon, ChevronRightIcon} from '@heroicons/react/24/outline'
 import WhatsApp from "../../Components/WhatsApp";
 import { withBase } from "../../Utils/path";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 //OPCION DE COLORES 2.1
 //#DDE7E6 #ADD2DA #378BA1 #F8EEDD #FFDCB6 #D2B387
 //Hover #286A77
 //Hover bright #EEF4F3
 //text #256A77
+const responsive = {
+  desktop: {breakpoint: { max: 3500, min: 1024 }, items: 1, },
+  tablet: {breakpoint: { max: 1024, min: 464 }, items: 1, },
+  mobile: {breakpoint: { max: 464, min: 0 }, items: 1, },
+};
+
 const categories = [
-  { key: 'sanJose', name: 'San José del Cabo', image: '/assets/images/city.jpg' },
-  { key: 'losCabos', name: 'Cabo San Lucas', image: '/assets/images/cabos/cabos1.jpg' },
-  { key: 'laPaz', name: 'La Paz', image: '/assets/images/cabos/Picture6.jpg' },
-  { key: 'todosSantos', name: 'Todos Santos', image: '/assets/images/cabos/Picture15.png' }
+  { key: 'sanJose', name: 'San José del Cabo', image:['/assets/images/city.jpg','/assets/images/cabos/sanJose2.png','/assets/images/cabos/sanJose3.png','/assets/images/cabos/sanJose5.png'] },
+  { key: 'losCabos', name: 'Cabo San Lucas', image:['/assets/images/cabos/cabos1.png','/assets/images/cabos/cabos2.png','/assets/images/cabos/cabos3.png'] },
+  { key: 'laPaz', name: 'La Paz', image:['/assets/images/cabos/Picture6.jpg'] },
+  { key: 'todosSantos', name: 'Todos Santos', image:['/assets/images/cabos/Picture15.png','/assets/images/cabos/todosSantos2.png','/assets/images/cabos/todosSantos3.png','/assets/images/cabos/todosSantos4.png']}
 ];
+
 function places() {  
     const { language } = useContext(ToursContext);
     const content = contentData[language] || contentData['en']; 
@@ -31,7 +38,7 @@ function places() {
         return <div>Tours not found</div>;
     }
     const place = categories.find(cat => cat.key.toLowerCase() === id.toLowerCase());
-    console.log(place.key);
+    console.log(place.image);
     var texto1 = "1";
     var texto2 = ""
     switch (place.key) {
@@ -55,11 +62,40 @@ function places() {
     return (
         <Layout>
           <div className="font-[quicksand] w-full bg-gradient-to-b from-[#256A77]/15 to-[#fff]/90  flex flex-col justifty-center items-center">            
-            <div className='mb-4 h-screen bg-[white] pt-8 max-w-[800px] pl-4 pr-4 sm:pl-8 sm:pr-8 flex flex-col justifty-center items-center'>
-              <img
-                src={withBase(place.image)}
+            <div className='mb-4 h-screen bg-[white] pt-8 w-[100%] max-w-[800px] pl-4 pr-4 sm:pl-8 sm:pr-8 flex flex-col justifty-center items-center'>
+              {/*<img
+                src={withBase(place.image[0])}
                 className="w-[100%] h-auto object-contain"
-              />
+              />*/}
+              <Carousel 
+                infinite              
+                autoPlay={false}
+                className="w-[100%] h-[400px]"            
+                customRightArrow={
+                  <button
+                    className="absolute right-2 top-1/2 -translate-y-1/2 bg-[#ADD2DA]/50 text-white p-2 w-[45px] rounded-full shadow-lg hover:bg-[#ADD2DA] cursor-pointer"
+                  >
+                    <ChevronRightIcon size={18} />
+                  </button>
+                }
+                customLeftArrow={
+                <button
+                  className="absolute left-2 top-1/2 -translate-y-1/2 bg-[#ADD2DA]/50 text-white w-[45px] p-2 rounded-full shadow-lg hover:bg-[#ADD2DA] cursor-pointer"
+                >
+                  <ChevronLeftIcon size={20} />
+                </button>
+              }
+                responsive={responsive}>
+                  {place.image.map((src, index) => (
+                  <div key={src} className="w-[100%]">
+                      <img
+                      src={src}
+                      alt={`Car ${index + 1}`}
+                      className={`w-[100%] h-[400px] object-cover`}
+                      />
+                  </div>
+                  ))}
+              </Carousel>
               <h2 className="w-full text-3xl mt-6 pb-1 sm:text-4xl font-semibold drop-shadow-md mb-2">
                   {place.name}
               </h2>  
