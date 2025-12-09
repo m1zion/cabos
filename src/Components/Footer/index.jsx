929ea68bb000875b10c4382abcb9a1af812dffe8
unknown <misaelzion@gmail.com>
2025-12-09 17:48:07 -0600

    Adding metadata

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -81,7 +81,7 @@ const Footer = () =>{
                 </ul>                   
             </div>
             <div className="text-center text-sm mt-10 text-gray-400">
-                &copy; {new Date().getFullYear()} Cabos. All rights reserved.
+                &copy; {new Date().getFullYear()} Cabos. All rights reserved. v1.1
             </div>
     </footer>
     )
